188dde7a3898cb13edc20c430d8dc7323919ff2e
rlaehghks5 <rlaehghks5@gmail.com>
2023-05-10 07:10:59 +0000

    wandb problem solve

--- mmdetection/train.py
+++ mmdetection/train.py
@@ -19,54 +19,68 @@ def parse_arguments():
         help="config file for training",
     )
     return parser.parse_args()
- 
+
+
 classes = ("General trash", "Paper", "Paper pack", "Metal", "Glass", 
-           "Plastic", "Styrofoam", "Plastic bag", "Battery", "Clothing")
+           "Plastic", "Styrofoam", "Plastic bag", "Battery", "Clothing") # 10개의 클래스
 
 args = parse_arguments()
 
 # config file 들고오기
 # cfg = Config.fromfile('./configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py')
 cfg = Config.fromfile(args.config_dir)
+cfg_name = 'faster_rcnn_r50_fpn_1x_coco'
+root='/opt/ml/dataset/'
 
-# wandb
-wandb.init(project = 'waste_detection', name = 'exp')
-cfg.log_config.hooks = [
-        dict(type='TextLoggerHook'),
-        dict(type='MMDetWandbHook',
-             init_kwargs = {'project' : 'waste_detection'},
-         interval=10,
-         log_checkpoint=True,
-         log_checkpoint_metadata=True,
-         num_eval_images=100
-        )
-        # dict(type='TensorboardLoggerHook')
-]
-
-root='../../dataset/'
-
-# dataset config 수정
+# dataset.py 수정
 cfg.data.train.classes = classes
 cfg.data.train.img_prefix = root
-cfg.data.train.ann_file = root + 'train.json' # train json 정보
-cfg.data.train.pipeline[2]['img_scale'] = (512,512) # Resize
+cfg.data.train.ann_file = root + 'train_data_fold_0_seed_411.json' # train json 정보
+cfg.data.train.pipeline[2]['img_scale'] = (512, 512) # Resize
+
+# dataset.py 수정
+cfg.data.val.classes = classes
+cfg.data.val.img_prefix = root
+cfg.data.val.ann_file = root + 'val_data_fold_0_seed_411.json' # valid json 정보
+cfg.data.val.pipeline[1]['img_scale'] = (512, 512) # Resize
 
 cfg.data.test.classes = classes
 cfg.data.test.img_prefix = root
 cfg.data.test.ann_file = root + 'test.json' # test json 정보
-cfg.data.test.pipeline[1]['img_scale'] = (512,512) # Resize
+cfg.data.test.pipeline[1]['img_scale'] = (512, 512) # Resize
 
-cfg.data.samples_per_gpu = 4
+cfg.data.samples_per_gpu = 16
 
-cfg.seed = 2022
+# train.py 수정
+cfg.seed = 909
+cfg.deterministic = True # 찾아보자
 cfg.gpu_ids = [0]
+cfg.work_dir = f'./work_dirs/{cfg_name}_trash'
 
-cfg.work_dir = f"./work_dirs/{args.config_dir.split('/')[-1][:-3]}"
-
+# retinanet_r50_fpn.py 수정
+# cfg.model.bbox_head.num_classes = 10
 cfg.model.roi_head.bbox_head.num_classes = 10
 
-cfg.optimizer_config.grad_clip = dict(max_norm=35, norm_type=2)
+# schedule_1x.py 수정
+# cfg.optimizer = dict(type='Adam', lr=2e-3, weight_decay=0.0001)
+cfg.optimizer_config = dict(grad_clip = dict(max_norm=35, norm_type=2)) # Use gradient clip to stabilize training
+
+# default_runtime.py 수정
 cfg.checkpoint_config = dict(max_keep_ckpts=3, interval=1)
+cfg.log_config.interval = 50
+cfg.log_config.hooks.append(
+                            dict(type='WandbLoggerHook',
+                                 interval=50,
+                                init_kwargs=dict(
+                                       project = 'waste_detection',
+                                       name = f'dh){cfg_name}',
+                                   )))
+
+meta = dict()
+# fp16 settings
+# cfg.fp16 = dict(loss_scaler=[512.])
+
+cfg.workflow = [('train', 1)]
 cfg.device = get_device()
 
 # build_dataset
@@ -77,4 +91,4 @@ model = build_detector(cfg.model)
 model.init_weights()
 
 # 모델 학습
-train_detector(model, datasets[0], cfg, distributed=False, validate=False)
+train_detector(model, datasets[0], cfg, distributed=False, validate=True)
